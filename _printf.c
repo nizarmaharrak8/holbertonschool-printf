4659e28976b329bbc0f1e8e620efb0b57f1bb02c
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-11-18 15:47:29 +0000

    add

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i = 0;
+    char c;
+
+    if (!format || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
+
+    va_start(args, format);
+
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+
+            if (format[i] == 'c')
+            {
+                c = va_arg(args, int);
+                write(1, &c, 1);
+            }
+        }
+        else
+        {
+            write(1, &format[i], 1);
+        }
+
+        i++;
+    }
+
+    va_end(args);
+    return (0);
+}
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+int main(void)
+{
+_printf("%");
+return(0);
+
+}
